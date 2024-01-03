8a84c9c95ab778abebaf3fcdaa073b8dc3f42ca3
ryo-ebata <r.ebata@hajimari.inc>
2024-01-03 13:13:54 +0900

    fix: if chain can be rewritten with match

--- src/main.rs
+++ src/main.rs
@@ -15,39 +15,43 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let pet: Pet;
 
-    // if user wrote command line with options
-    if args.len() > 1 {
-        match args[1].as_str() {
-            "--h" => options::help::help(),
-            _ => println!("Invalid option. Try --h for help."),
-        }
-    
-    // if user wrote just command line
-    } else if args.len() == 1 {
-        if Path::new("src/public/clipet.json").exists() {
-            let file = File::open("src/public/clipet.json").expect("Could not open file");
-            match serde_json::from_reader::<_, Pet>(file) {
-                Ok(data) => {
-                    println!("\nWELCOME BACK to CLI PET!\n");
-                    pet = data
-                },
-                Err(_) => {
-                    println!("The file does not contain valid Pet data.\n");
-                    systems::first_choise::first_choise();
-
-                    pet = fetch_pet();
-                }
+    match args.len() {
+         // if user wrote command line with options
+        n if n > 1 => {
+            match args[1].as_str() {
+                "--h" => options::help::help(),
+                _ => println!("Invalid option. Try --h for help."),
             }
-        } else {
-            systems::first_choise::first_choise();
-            pet = fetch_pet();
-        }
-        match pet.r#type.as_str() {
-            "cat" => options::animals::cat::cat(&pet.name),
-            "dog" => options::animals::dog::dog(&pet.name),
-            "bird" => options::animals::bird::bird(&pet.name),
-            _ => println!("Invalid option. Try --h for help."),
-        }
+        },
+
+        // if user wrote just command line
+        1 => {
+            if Path::new("src/public/clipet.json").exists() {
+                let file = File::open("src/public/clipet.json").expect("Could not open file");
+                match serde_json::from_reader::<_, Pet>(file) {
+                    Ok(data) => {
+                        println!("\nWELCOME BACK to CLI PET!\n");
+                        pet = data
+                    },
+                    Err(_) => {
+                        println!("The file does not contain valid Pet data.\n");
+                        systems::first_choise::first_choise();
+
+                        pet = fetch_pet();
+                    }
+                }
+            } else {
+                systems::first_choise::first_choise();
+                pet = fetch_pet();
+            }
+            match pet.r#type.as_str() {
+                "cat" => options::animals::cat::cat(&pet.name),
+                "dog" => options::animals::dog::dog(&pet.name),
+                "bird" => options::animals::bird::bird(&pet.name),
+                _ => println!("Invalid option. Try --h for help."),
+            }
+        },
+        _ => (),
     }
 }
 
